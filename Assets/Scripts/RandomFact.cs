bce9415bb12a13b9633c8f13fa593b981d1e1d7e
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2016-10-27 23:17:49 +0100

    Main user interfaces added

---
+++ Assets/Scripts/RandomFact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomFact : MonoBehaviour {
+
+   // string[] facts = ["um", "dois", "tres"];
+
+    public string getRandomFact()
+    {
+        Random r = new Random();
+        //Console.Write(r);
+
+        string fact = "fact fact fact";
+        return fact;
+    }
+
+    void OnGUI()
+    {
+        System.Console.Write("hello");
+        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+    }
+
+}
