a1a3b9d4e6852955dd817e4e118d09de1053f027
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2016-11-08 17:31:12 +0000

    Choice of dialogue implemented
    need to fix a few bugs

--- Assets/Scripts/Level1Scripts/Level1bManager.cs
+++ Assets/Scripts/Level1Scripts/Level1bManager.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System;
+using System.Collections.Generic;
 
 public class Level1bManager : MonoBehaviour {
 
@@ -27,12 +28,53 @@ public class Level1bManager : MonoBehaviour {
 	private Button answerD;
 
 	// Dialogue Variables
-	private string[] mainCharacterLines = {"Hey, new kid! Wanna join us playing football? We need one more player.", "Great!", "v!"};
-	private string[] cmtCharacterLines = {"Hey there, thanks. I would love to play with you, but I cannot play football.", "v"};
+	private string[] mainCharacterLines = {"Hey, new kid! Wanna join us playing football? We need one more player."};
+	private string[] cmtCharacterLines = {"Hey there, thanks. I would love to play with you, but I cannot play football."};
 	private string[] choices = {"yes", "no"};
 
+	private string[] questionChoices1 = {
+		"Why? Is there something wrong?",
+		"Why not? You don't look sick or anything."
+	};
+
+	private string[]answers1 = {
+		"Yes, I have a disease called Charcot-Marie-Tooth. It makes it really hard for me to maintain balance and control my body like you.",
+	 	"You shouldn't assume things just by looking at people. I'm not sick, but I actually have this disease called Charcot-Marie-Tooth. It makes it really hard for me to maintain balance and control my body like you."
+	};
+
+	private string[] questionChoices2 = {
+		"Oh, I'm sorry to hear that. If you want, I can ask the guys to play something other than football so you can join us." ,
+	 	"Oh, I'm sorry to hear that. I guess we'll have to play with an odd number of players, then. See you later!",
+	  "You can be the goalkeeper, then. You won't have to move too much and we get to play with an even number of players.",
+	 	"I guess no one will want to play with you then. I'll try to find someone else."
+	};
+
+	private string[] answers2 = {
+		"Thanks. I really appreciate it.",
+		"Thanks. See you later then.",
+		"I think I'll pass.",
+		"That was not very nice from you. You seemed like a respectful guy at first."
+	};
+
+	private string[] questionChoices3 = {
+		"I'm sorry I said that. That was rude. If you want, I can ask the guys to play something other than football so you can join us.",
+		"I'm sorry I said that. That was rude. I guess we'll have to play with an odd number of players, then. See you later!",
+		"You can be the goalkeeper, then. You won't have to move too much and we get to play with an even number of players.",
+		"Who cares? No one will want to play with you, then. I'll try to find someone else."
+	};
+
+	private string[] answers3 = {
+		"No problem, I don't think you meant to be rude. I'd really appreciate it if you guys played something else just so I can play too.",
+	 	"Sure, no problem. See you later, then.",
+		"No thanks. I'd rather do something else.",
+		"You are really rude."
+	};
+
+	private List<string[]> questionsList;
+	private List<string[]> cmtCharacterSpeechList;
+
 	// Control Variables
-	bool mainCharacterTurn;
+	private bool mainCharacterTurn;
 	private bool displayChoices = true;
 	private bool displayNextButton = false;
 	private bool interactionOver;
@@ -43,7 +85,7 @@ public class Level1bManager : MonoBehaviour {
 	private GUIStyle style, continueStyle;
 
 
-	/////////////////////////////////////////////////////////////////////////////
+	// Start interaction
 	void Start() {
 
 		// initialize control variables
@@ -51,6 +93,7 @@ public class Level1bManager : MonoBehaviour {
 		cmtCharacterDialogueCounter = 0;
 		mainCharacterTurn = true;
 		interactionOver = false;
+		questionsIterator = 0;
 
 		// Get Character's Lines
 		//  XMLParser("level1, mainCharacterLines ,cmtCharacterLines");
@@ -75,40 +118,27 @@ public class Level1bManager : MonoBehaviour {
 		mainCharacterSpeechBalloon.enabled = false;
 		cmtCharacterSpeechBalloon.enabled = false;
 
-		// Hide response buttons
-		answerA.gameObject.SetActive(false);
-		answerB.gameObject.SetActive(false);
-		answerC.gameObject.SetActive(false);
-		answerD.gameObject.SetActive(false);
+		// initialize questions and answers
+		// Should be done with the xmlParser
+	 questionsList = new List<string[]>();
+	 questionsList.Add(questionChoices1);
+	 questionsList.Add(questionChoices2);
+	 questionsList.Add(questionChoices3);
 
-		// Format buttons
-		formatGUI();
+	 cmtCharacterSpeechList = new List<string[]>();
+	 cmtCharacterSpeechList.Add(answers1);
+	 cmtCharacterSpeechList.Add(answers2);
+	 cmtCharacterSpeechList.Add(answers3);
+
+		// Hide response buttons
+		hideButtons();
 
 		// start actions
 		continueDialogue();
 	}
 
-	// Clear Speech Balloon
-	void clearText(Text textObject) {
-		textObject.text = "";
-	}
-
-	void interactionCycle() {
-		if (mainCharacterDialogueCounter == mainCharacterLines.Length) {
-			displayChoices = false;
-			displayNextButton = true;
-		} else {
-			displayChoices = true;
-		}
-	}
-
 	// Dialogue cycle
 	void continueDialogue() {
-		if (mainCharacterDialogueCounter == mainCharacterLines.Length-1 && cmtCharacterDialogueCounter == cmtCharacterLines.Length-1) {
-			interactionOver = true;
-			return;
-		}
-
 		if (mainCharacterTurn) {
 			// clear everything
 			cmtCharacterSpeechBalloon.enabled = false;
@@ -131,6 +161,69 @@ public class Level1bManager : MonoBehaviour {
 		mainCharacterTurn = !mainCharacterTurn;
 	}
 
+
+	private int questionsIterator;
+	void askQuestions() {
+		Debug.Log("questionsIterator: " + questionsIterator);
+		loadQuestions(questionsList[questionsIterator]);
+
+	}
+
+
+	void getCMTresponse(int number) {
+		// Clean Everything
+		clearText(cmtCharacterSpeech);
+		// Write Text
+		message =	cmtCharacterSpeechList[questionsIterator][number];
+		StartCoroutine(writeCMTspeech(message));
+	}
+
+
+
+	void loadQuestions(string[] arr) {
+
+		answerA.GetComponentInChildren<Text>().text = arr[0];
+		answerA.onClick.AddListener( () => buttonClicked(0));
+		answerA.gameObject.SetActive(true);
+
+		answerB.GetComponentInChildren<Text>().text = arr[1];
+		answerB.onClick.AddListener( () => buttonClicked(1));
+		answerB.gameObject.SetActive(true);
+
+		if (arr.Length > 2) {
+			answerC.GetComponentInChildren<Text>().text = arr[2];
+			answerC.onClick.AddListener( () => buttonClicked(2));
+			answerC.gameObject.SetActive(true);
+
+		}
+		if (arr.Length == 4) {
+			answerD.GetComponentInChildren<Text>().text = arr[2];
+			answerD.onClick.AddListener( () => buttonClicked(3));
+			answerD.gameObject.SetActive(true);
+		}
+	}
+
+	// Hide buttons
+	void hideButtons() {
+		answerA.gameObject.SetActive(false);
+		answerB.gameObject.SetActive(false);
+		answerC.gameObject.SetActive(false);
+		answerD.gameObject.SetActive(false);
+	}
+
+	// Clear Speech Balloon
+	void clearText(Text textObject) {
+		textObject.text = "";
+	}
+
+
+	void buttonClicked(int buttonNumber) {
+		Debug.Log("chose option: " + buttonNumber);
+		hideButtons();
+		getCMTresponse(buttonNumber);
+	}
+
+
 	void OnGUI() {
 		if (displayNextButton) {
 			if (GUI.Button(new Rect(375, 225, 268, 25), "Continue")) {
@@ -139,35 +232,51 @@ public class Level1bManager : MonoBehaviour {
 			}
 		}
 		if (interactionOver){
+			hideButtons();
 			if (GUI.Button(new Rect(375, 225, 268, 25), "Return")) {
 				SceneManager.LoadScene("SchoolOutdoors");
 			}
 		}
-
 	}
 
 
 	// Write text to screen
 	IEnumerator TypeText (string message) {
 		if (mainCharacterTurn) {
+			mainCharacterSpeechBalloon.enabled = true;
 			for (int i = 0; i < message.Length; i++) {
-				mainCharacterSpeechBalloon.enabled = true;
 				mainCharacterSpeech.text += message[i];
 				yield return new WaitForSeconds (letterPause);
 			}
-		} else {
+		} else{
 			for (int i = 0; i < message.Length; i++) {
 				cmtCharacterSpeechBalloon.enabled = true;
 				cmtCharacterSpeech.text += message[i];
 				yield return new WaitForSeconds (letterPause);
 			}
 		}
+		if (mainCharacterDialogueCounter == mainCharacterLines.Length && cmtCharacterDialogueCounter == cmtCharacterLines.Length) {
+			askQuestions();
+		} else
 		displayNextButton = true;
 	}
 
-	void formatGUI()	{
-		//style.fontSize = 14;
-		//	style.font = (Font)Resources.Load("Fonts/Jekyll"); ;
+	IEnumerator writeCMTspeech (string message) {
+		cmtCharacterSpeech.text = "";
+		cmtCharacterSpeechBalloon.enabled = true;
+		for (int i = 0; i < message.Length; i++) {
+			cmtCharacterSpeech.text += message[i];
+			yield return new WaitForSeconds (letterPause);
+		}
+		if (questionsIterator < (questionsList.Count-1)){
+			questionsIterator++;
+			askQuestions();
+		}
+		else {
+			Debug.Log("acabou");
+			interactionOver = true;
+		}
+
 	}
 
 	// Update is called once per frame
