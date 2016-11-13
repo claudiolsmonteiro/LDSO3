a80dd4365c3e36dc9a500d0490c51ba2840c1acf
Fábio Amarante <fabioamarante@hotmail.com>
2016-11-13 02:05:44 +0000

    Started mobile optimization. Various fixes on scenes

--- Assets/Scripts/RandomFact.cs
+++ Assets/Scripts/RandomFact.cs
@@ -5,37 +5,25 @@ using System;
 
 public class RandomFact : MonoBehaviour {
 
-    private string[] facts = { "um", "CMT is very dangerous", "tres" };
-    private string fact;
-    private System.Random rand;
-    private GUIStyle style;
+    private string[] _facts = { "um", "CMT is very dangerous", "tres" };
+    private string _fact;
+    private System.Random _rand;
+    private GameObject _textObject;
+    private Text _text;
 
     void Start ()
     {
         // Initialize Variables
-        rand = new System.Random();
-        style = new GUIStyle();
+        _rand = new System.Random();
+        _textObject = GameObject.Find("Canvas/Content/Random Fact");
+        _text = _textObject.GetComponent<Text>();
 
         // Generate Random Number
-        int r = rand.Next(0, facts.Length);
-        fact = r + ": " + facts[r];
+        int r = _rand.Next(0, _facts.Length);
+        _fact = r + ": " + _facts[r];
 
-        // Format GUI
-        formatGUI();
-    
-    }
-
-    void formatGUI()
-    {
-        style.fontSize = 14;
-        style.font = (Font)Resources.Load("Fonts/Jekyll"); ;
-    }
-
-
-    void OnGUI()
-    {
-                
-        GUI.Box(new Rect(395, 180, 268, 75), fact, style);
+        // Set random fact text
+        _text.text = _fact;
     }
 
 }
