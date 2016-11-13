83f733bc59f0e2ffd040952bafafc4ac883b26ee
Fábio Amarante <fabioamarante@hotmail.com>
2016-11-13 18:50:30 +0000

    Camera follows player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform leftBorder;
+    public Transform rightBorder;
+    public Transform bottomBorder;
+    public Transform upperBorder;
+    private Transform character;
+
+    void Start()
+    {
+        character = GameObject.Find("Character").transform;
+    }
+
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = character.position.x;
+        newPosition.x = Mathf.Clamp(newPosition.x, leftBorder.position.x, rightBorder.position.x);
+        newPosition.y = character.position.y;
+        newPosition.y = Mathf.Clamp(newPosition.y, bottomBorder.position.y, upperBorder.position.y);
+    
+        transform.position = newPosition;
+    }
+}
