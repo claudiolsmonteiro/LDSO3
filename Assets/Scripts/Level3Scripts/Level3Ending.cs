2af41bb3d721a854bab689e245cb3b6de6edb07a
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2016-12-21 13:19:12 +0000

    Level 3 ending fix

---
+++ Assets/Scripts/Level3Scripts/Level3Ending.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Level3CollisionScript : MonoBehaviour
+{
+    public void OnTriggerEnter2D()
+    {
+        Time.timeScale = 0;
+        Debug.Log("ouchie");
+        SceneManager.LoadScene("Finished");
+
+
+    }
+
+
+}
